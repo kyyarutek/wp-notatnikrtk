81b9d62c5a29b9ee07a6989ba45095a681c83f4b
kyyarutek <kyyarutek@gmail.com>
2015-06-13 10:47:47 +0200

    commit notatnik

---
+++ NotatnikRTK/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace NotatnikRTK
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var appStorage = IsolatedStorageFile.GetUserStoreForApplication();
+            string fileName = "simple.txt";
+            using (var file = appStorage.OpenFile(fileName, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write))
+            {
+                using (var writer = new StreamWriter(file))
+                {
+                    writer.Write(textBox1.Text);
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+            { 
+            using(StreamReader sr= new StreamReader(store.OpenFile("simple.txt", FileMode.Open, FileAccess.Read)))
+            {
+                textBox1.Text = sr.ReadToEnd();
+            
+            }
+            
+            }
+        }
+        }
+    }
---
+++ NotatnikRTK/Start.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace NotatnikRTK
+{
+    public partial class Start : PhoneApplicationPage
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Created by Rutkowski");
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
